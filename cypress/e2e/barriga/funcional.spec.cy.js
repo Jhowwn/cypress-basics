6db96d99658d06e1b23361309561dd96c6947ea0
LetsWorkb1 <jhow.win@gmail.com>
2023-01-30 16:55:14 -0300

    Finalization to funcional tests and create commands

--- cypress/e2e/barriga/funcional.spec.cy.js
+++ cypress/e2e/barriga/funcional.spec.cy.js
@@ -1,32 +1,73 @@
 /// <reference types="cypress" />
+import '../../support/commandsContas';
 import { locators } from '../../support/locators';
 
 describe('Making tests at a functional level', () => {
-  before(() => {
+  beforeEach(() => {
     cy.login('testfy', '123')
     cy.resetApp()
   })
 
-  // beforeEach(() => {
-  //   cy.reload()
-  // })
-
   it('Should create an account', () => {
-    cy.get(locators.MENU.SETTINGS).click()
-    cy.get(locators.MENU.CONTAS).click()
-    cy.get(locators.CONTAS.NOME).type('Conta nova')
-    cy.get(locators.CONTAS.BTN_SALVAR).click()
+    cy.acessarMenuConta()
+    cy.inserirConta('Conta nova')
     cy.get(locators.MESSAGE).should('contain', 'Conta inserida com sucesso!')
   })
 
-  it.only('Should update an account', () => {
-    cy.get(locators.MENU.SETTINGS).click()
-    cy.get(locators.MENU.CONTAS).click()
-    cy.xpath(locators.CONTAS.XP_BTN_ALTERAR).click()
+  it('Should update an account', () => {
+    cy.acessarMenuConta()
+    cy.xpath(locators.CONTAS.FN_XP_BTN_ALTERAR('Conta para alterar')).click()
     cy.get(locators.CONTAS.NOME)
       .clear()
       .type('Conta alterada')
     cy.get(locators.CONTAS.BTN_SALVAR).click()
     cy.get(locators.MESSAGE).should('contain', 'Conta atualizada com sucesso!')
+  })
+
+  it('Should not create an account with same name', () => {
+    cy.acessarMenuConta()
+
+    cy.get(locators.CONTAS.NOME).type('Conta mesmo nome')
+    cy.get(locators.CONTAS.BTN_SALVAR).click()
+
+    cy.get(locators.MESSAGE).should('contain', 'code 400')
+  })
+
+  it('Should create a transaction', () => {
+    cy.get(locators.MENU.MOVIMENTACAO).click()
+    cy.get(locators.MOVIMENTACAO.DESCRICAO).type('Desc')
+    cy.get(locators.MOVIMENTACAO.VALOR).type(123)
+    cy.get(locators.MOVIMENTACAO.INTERESSADO).type('Inter')
+    cy.get(locators.MOVIMENTACAO.CONTA).select('Conta para movimentacoes')
+    cy.get(locators.MOVIMENTACAO.STATUS).click()
+    cy.get(locators.MOVIMENTACAO.BTN_SALVAR).click()
+
+    cy.get(locators.MESSAGE).should('contain', 'sucesso')
+
+    cy.get(locators.EXTRATO.LINHAS).should('have.length', 7)
+    cy.xpath(locators.EXTRATO.FN_XP_BUSCA_ELEMENTO('Desc', '123')).should('exist')
+  })
+
+  it.only('Should get balance', () => {
+    cy.get(locators.MENU.HOME).click()
+    cy.xpath(locators.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '534,00')
+
+    cy.get(locators.MENU.EXTRATO).click()
+    cy.xpath(locators.EXTRATO.FN_XP_ALTERAR_ELEMENTO('Movimentacao 1, calculo saldo')).click()
+    cy.get(locators.MOVIMENTACAO.DESCRICAO).should('have.value', 'Movimentacao 1, calculo saldo')
+    cy.get(locators.MOVIMENTACAO.STATUS).click()
+    cy.get(locators.MOVIMENTACAO.BTN_SALVAR).click()
+
+    cy.wait(1000)
+    cy.get(locators.MESSAGE).should('contain', 'sucesso')
+
+    cy.get(locators.MENU.HOME).click()
+    cy.xpath(locators.SALDO.FN_XP_SALDO_CONTA('Conta para saldo')).should('contain', '4.034,00')
+  })
+
+  it('Should remova a transaction', () => {
+    cy.get(locators.MENU.EXTRATO).click()
+    cy.xpath(locators.EXTRATO.FN_XP_REMOVER_ELEMENTO('Movimentacao para exclusao')).click()
+    cy.get(locators.MESSAGE).should('contain', 'sucesso')
   })
 })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -5,14 +5,34 @@ export const locators = {
     BTN: '.btn'
   },
   MENU: {
+    HOME: '[data-test="menu-home"]',
     SETTINGS: '[data-test="menu-settings"]',
     CONTAS: '[href="/contas"]',
-    RESET: '[href="/reset"]'
+    RESET: '[href="/reset"]',
+    EXTRATO: '[data-test="menu-extrato"]',
+    MOVIMENTACAO: '[data-test="menu-movimentacao"]'
   },
   CONTAS: {
-    NOME: '[href="/contas"]',
+    NOME: '[data-test="nome"]',
     BTN_SALVAR: '.btn',
-    XP_BTN_ALTERAR: "//table//td[contains(., 'Conta nova')]/..//i[@class='far fa-edit']"
+    FN_XP_BTN_ALTERAR: nome => `//table//td[contains(., '${nome}')]/..//i[@class='far fa-edit']`
+  },
+  MOVIMENTACAO: {
+    DESCRICAO: '[data-test="descricao"]',
+    VALOR: '[data-test="valor"]',
+    INTERESSADO: '[data-test="envolvido"]',
+    CONTA: '[data-test="conta"]',
+    STATUS: '[data-test="status"]',
+    BTN_SALVAR: '.btn-primary'
+  },
+  EXTRATO: {
+    LINHAS: '.list-group > li',
+    FN_XP_BUSCA_ELEMENTO: (desc, value) => `//span[contains(., '${desc}')]/following-sibling::small[contains(., '${value}')]`,
+    FN_XP_REMOVER_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='far fa-trash-alt']`,
+    FN_XP_ALTERAR_ELEMENTO: conta => `//span[contains(., '${conta}')]/../../..//i[@class='fas fa-edit']`
+  },
+  SALDO: {
+    FN_XP_SALDO_CONTA: nome => `//td[contains(., '${nome}')]/../td[2]`
   },
   MESSAGE: '.toast-message',
 }
